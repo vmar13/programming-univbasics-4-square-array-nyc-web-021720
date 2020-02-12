d6f24f16e69fb8c1abc0e2ed2990c6517d473982
Vanessa Martinez <vmar76@gmail.com>
2020-02-12 00:37:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,9 @@ def square_array(array)
   results=[]
   counter = 0
   while counter < array.size do
-  	## square the current number and push it into results
     
     current_number = array[counter]
+    results << current_number ** 2 
     counter += 1
   end
   
